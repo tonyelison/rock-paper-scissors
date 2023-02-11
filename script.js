d4d9d3c77e8981950e27e9878c44e7a0d978f187
Anthony Elison <tonyelison37@gmail.com>
2023-02-11 16:14:51 -0700

    Compute winner and log the result in the console

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-const choices = ['rock', 'paper', 'scissors'];
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+const CHOICES = [ROCK, PAPER, SCISSORS];
 
 function getComputerChoice() {
   let index = Math.floor(Math.random() * 3); // 0, 1, or 2
-  return choices[index];
+  return CHOICES[index];
 }
 
 function getPlayerChoice() {
   let choice = prompt("Rock, Paper, or Scissors?");
   const parseChoice = () => choice.toLowerCase();
   
-  while (!choices.includes(parseChoice())) {
+  while (!CHOICES.includes(parseChoice())) {
     choice = prompt(`Whaddya mean, "${parseChoice()}"? Don't you know how to play this game? Rock, Paper, or Scissors?!`);
   }
 
   return parseChoice();
-
 }
 
 function playRound(playerSelection, computerSelection) {
-  console.log(playerSelection);
-  console.log(computerSelection);
+  if (playerSelection === computerSelection) {
+    return `Tie! Both players chose ${playerSelection}.`;
+  }
+
+  const outcome = [playerSelection, computerSelection];
+  const playerWinScenarios = [
+    [ROCK, SCISSORS],
+    [PAPER, ROCK],
+    [SCISSORS, PAPER]
+  ];
+
+  if (playerWinScenarios.find((scenario) => {
+    return scenario[0] === outcome[0] && scenario[1] === outcome[1];
+  })) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
 }
 
-playRound(getPlayerChoice(), getComputerChoice());
+const playerChoice = getPlayerChoice();
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
