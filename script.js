422f3dd830ce768cf558968955a846015aa5239b
Anthony Elison <tonyelison37@gmail.com>
2023-02-11 15:43:25 -0700

    Prompt user for selection, handle invalid input

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
   let index = Math.floor(Math.random() * 3); // 0, 1, or 2
   return choices[index];
 }
 
+function getPlayerChoice() {
+  let choice = prompt("Rock, Paper, or Scissors?");
+  const parseChoice = () => choice.toLowerCase();
+  
+  while (!choices.includes(parseChoice())) {
+    choice = prompt(`Whaddya mean, "${parseChoice()}"? Don't you know how to play this game? Rock, Paper, or Scissors?!`);
+  }
+
+  return parseChoice();
+
+}
+
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection);
+  console.log(computerSelection);
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
